ff4084e2a113066ddcfc884685b258ce3165b29c
Jesse Lingeman <jmlingeman@gmail.com>
2014-05-06 16:33:58 -0400

    yay more indexes

--- TestFramework/TestIndexes.py
+++ TestFramework/TestIndexes.py
@@ -1,7 +1,10 @@
+import time
+
 from TestFramework.Disk import Disk
 from TestFramework.HashIndex import HashIndex
 from DataParser import read_trajectory_data
 from BTree import BPlusTree
+
 
 __author__ = 'jesse'
 
@@ -30,9 +33,31 @@ data, names, id_to_num, num_to_id, pid_to_place, place_to_pid = read_trajectory_
 
 # Create a simple BTree of id => trajectory
 # Start with an order 50
+print "Creating id => trajectory btree"
 btree = BPlusTree(150)
 for d in data:
     btree.insert(d[0], d[1])
+
+# Create an index of location => ids
+print "Creating location => id btree"
+stime = time.time()
+btree_loc_to_id = BPlusTree(150)
+for d in data:
+    for t in d[1]:
+        btree_loc_to_id.insert(t, d[0])
+print time.time() - stime
+
+print "Creating location => id + idx btree"
+stime = time.time()
+btree_loc_to_id_idx = BPlusTree(150)
+for d in data:
+    for i, t in enumerate(d[1]):
+        btree_loc_to_id_idx.insert(t, (d[0], i))
+print time.time() - stime
+
+print "Retrieving list of things"
+print btree_loc_to_id_idx.getlist(names[0])
+
 
 # print btree.get(1)
 
