f0b904d0f3b6af9906335007a462f098fbc14e9a
Jesse Lingeman <jmlingeman@gmail.com>
2014-05-06 13:50:39 -0400

    Datareader now returns maps for the IDs for each thing

--- TestFramework/DataParser.py
+++ TestFramework/DataParser.py
@@ -9,4 +9,30 @@ def read_trajectory_data(input_file):
         traj = line[len("./dOxIc2o4o8zutl9hXW.M") + 1:].strip()
         traj = traj.split(",")
         data.append([id, traj])
-    return data
+
+    # Go through the data and build a hash for each ID
+    id_to_num = {}
+    num_to_id = {}
+    for i, d in enumerate(data):
+        id_to_num[d[0]] = i
+        num_to_id[i] = d[0]
+        d[0] = i
+    print data[0]
+    print len(data)
+
+    # Now get the list of names and build a map of those in alphabetical order
+    names = set()
+    # print data[0][1]
+    for d in data:
+        for t in d[1]:
+            names.add(t)
+    names = list(names)
+    names.sort()
+
+    pid_to_place = {}
+    place_to_pid = {}
+    for i, d in enumerate(data):
+        pid_to_place[d[0]] = i
+        place_to_pid[i] = d[0]
+
+    return data, names, id_to_num, num_to_id, pid_to_place, place_to_pid
--- TestFramework/TestIndexes.py
+++ TestFramework/TestIndexes.py
@@ -26,16 +26,7 @@ hash_index.get(2)
 hash_index.print_status()
 
 # Teat reading in the trajectory data
-data = read_trajectory_data("../../postFSM.txt")
-# Go through the data and build a hash for each ID
-id_to_num = {}
-num_to_id = {}
-for i, d in enumerate(data):
-    id_to_num[d[0]] = i
-    num_to_id[i] = d[0]
-    d[0] = i
-print data[0]
-print len(data)
+data, names, id_to_num, num_to_id, pid_to_place, place_to_pid = read_trajectory_data("../../postFSM.txt")
 
 # Create a simple BTree of id => trajectory
 # Start with an order 50
@@ -43,5 +34,5 @@ btree = BPlusTree(150)
 for d in data:
     btree.insert(d[0], d[1])
 
-print btree.get(1)
+# print btree.get(1)
 
